af85834ed5ee54fe627b368414b1dec1423af7fb
nisha <nisha.p@mobilestyx.co.in>
2023-10-09 10:48:05 +0530

    minor changes

--- ajax/dashboard/dashboard.js
+++ ajax/dashboard/dashboard.js
@@ -1,53 +1,69 @@
-$(document).ready(function () {
-   $.ajax({
-      url: "getUser.php",
-      method: 'GET',
-      dataType: 'json',
-      success: function (response) {
-         var userType = response.userType;
-         console.log(userType);
-   
-         // Update the navbar with the user's name
-         $('#userName').text('Welcome ' + response.name);
+// Function to redirect to the update page with the id parameter
+function redirectToUpdate(id) {
+   // Construct the URL with the id parameter
+   var updateUrl = 'http://localhost/siddhesh/ajax/update_info/update_info.html?id=' + id;
 
-         // Initialize the DataTable with custom rendering based on isAdmin
-         var table = $('#userTable').DataTable({
-            "ajax": {
-               "url": "dashboard.php",
-               "dataSrc": "data"
-            },
-            "columns": [
-               { "data": "id" },
-               { "data": "name" },
-               { "data": "email" },
-               { "data": "mobile" },
-               { "data": "gender" },
-               { "data": "userType" },
-               {
-                  "data": null,
-                  "render": function (data, type, row) {
-                     // alert(userType);
-                     if(userType === 'admin'){
-                        
-                        // Check if the user is an admin, show the action icons for admin users
-                        return '<td><i class="bi bi-pencil-square" style="color: blue"></i></td>' + "<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>" + 
-                        '<td> <i class="bi bi-trash" style="color: red"></i></td>';
+   // Redirect to the update page with the id parameter
+   window.location.href = updateUrl;
+}
 
-                     }else{
-                        return '<td></td><td></td><td></td>';
-                     }
+// Function for deleting an item (you may define this separately)
+function deleteItem(id) {
+   // Implement the delete logic here
+}
+
+
+$.ajax({
+   url: "getUser.php",
+   method: 'GET',
+   dataType: 'json',
+   success: function (response) {
+      var userType = response.userType;
+      console.log(userType);
+
+      // Update the navbar with the user's name
+      $('#userName').text('Welcome ' + response.name);
+
+      // Initialize the DataTable with custom rendering based on isAdmin
+      var table = $('#userTable').DataTable({
+         "ajax": {
+            "url": "dashboard.php",
+            "dataSrc": "data"
+         },
+         "columns": [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "email" },
+            { "data": "mobile" },
+            { "data": "gender" },
+            { "data": "userType" },
+            {
+               "data": null,
+               "render": function (data, type, row) {
+                  // alert(userType);
+                  if (userType === 'admin') {
+
+                     // Check if the user is an admin, show the action icons for admin users
+                     var updateButton = '<button class="btn btn-primary" onclick="redirectToUpdate(' + row.id + ')">Update</button>';
+                     var deleteButton = '<button class="btn btn-danger" onclick="deleteItem(' + row.id + ')">Delete</button>';
+                     return updateButton + " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; " + deleteButton;
+
+
+                  } else {
+                     return '<td></td><td></td><td></td>';
                   }
                }
-            ]
-         });
+            }
+         ]
+      });
 
-         if(userType !== 'admin'){
-            table.columns(6).visible(false);
-         }
-
-      },
-      error: function () {
-         console.log("Failed to fetch user name");
+      if (userType !== 'admin') {
+         table.columns(6).visible(false);
       }
-   });
+
+   },
+   error: function () {
+      console.log("Failed to fetch user name");
+   }
 });
+
--- ajax/registration/register.php
+++ ajax/registration/register.php
@@ -1,6 +1,51 @@
 <?php
-// Connect to your MySQL database
+// Include the database connection
 include_once '../database/db.php';
+
+// Define validation functions
+function validateName($name) {
+    // Name should not be empty and should contain only letters and spaces
+    return !empty($name) && preg_match("/^[a-zA-Z ]+$/", $name);
+}
+
+function validateEmail($email) {
+    // Use PHP's built-in email validation function
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validatePassword($password) {
+    // Password should be at least 8 characters long
+    return strlen($password) >= 8;
+}
+
+function validateMobile($mobile) {
+    // Mobile number should be a valid format, e.g., 123-456-7890
+    return preg_match("/^\d{3}-\d{3}-\d{4}$/", $mobile);
+}
+
+function validateGender($gender) {
+    // Gender can be validated according to your specific requirements
+    // For simplicity, we'll assume it's valid if it's "Male" or "Female"
+    return in_array($gender, ["Male", "Female"]);
+}
+
+function validateUserType($userType) {
+    // User type can be validated according to your specific requirements
+    // For simplicity, we'll assume it's valid if it's "User" or "Admin"
+    return in_array($userType, ["User", "Admin"]);
+}
+
+// Define a function to log messages to a file
+function logMessage($message) {
+    // Define the log file path
+    $logFile = 'registration_log.log';
+
+    // Append the message with a timestamp
+    $logMessage = date('Y-m-d H:i:s') . ' - ' . $message . "\n";
+
+    // Write the message to the log file
+    error_log($logMessage, 3, $logFile);
+}
 
 // Retrieve data from the AJAX request
 $name = $_POST["name"];
@@ -10,27 +55,47 @@ $mobile = $_POST["mobile"];
 $gender = $_POST["gender"];
 $userType = $_POST["userType"];
 
-// Hash the password for security
-$passwordHash = hash("sha256", $password);
+// Validate the input data
+if (
+    validateName($name) &&
+    validateEmail($email) &&
+    validatePassword($password) &&
+    validateMobile($mobile) &&
+    validateGender($gender) &&
+    validateUserType($userType)
+) {
+    // Hash the password for security
+    $passwordHash = hash("sha256", $password);
 
-// Use prepared statements to prevent SQL injection
-$stmt = $conn->prepare("INSERT INTO `ajax_user`(`name`, `email`, `password`, `mobile`, `gender`, `userType`) VALUES (?, ?, ?, ?, ?, ?)");
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("INSERT INTO `ajax_user`(`name`, `email`, `password`, `mobile`, `gender`, `userType`) VALUES (?, ?, ?, ?, ?, ?)");
 
-if ($stmt) {
-    // Bind parameters
-    $stmt->bind_param("ssssss", $name, $email, $passwordHash, $mobile, $gender, $userType);
+    if ($stmt) {
+        // Bind parameters
+        $stmt->bind_param("ssssss", $name, $email, $passwordHash, $mobile, $gender, $userType);
 
-    // Execute the statement
-    if ($stmt->execute()) {
-        echo "Registration successful!";
+        // Execute the statement
+        if ($stmt->execute()) {
+            echo "Registration successful!";
+            // Log a success message
+            logMessage("User registered successfully: $name, $email");
+        } else {
+            echo "Error: " . $stmt->error;
+            // Log an error message
+            logMessage("Error during registration: " . $stmt->error);
+        }
+
+        // Close the statement
+        $stmt->close();
     } else {
-        echo "Error: " . $stmt->error;
+        echo "Error preparing statement: " . $conn->error;
+        // Log an error message
+        logMessage("Error preparing statement: " . $conn->error);
     }
-
-    // Close the statement
-    $stmt->close();
 } else {
-    echo "Error preparing statement: " . $conn->error;
+    echo "Invalid input data. Please check your input and try again.";
+    // Log an error message
+    logMessage("Invalid input data: $name, $email, $mobile, $gender, $userType");
 }
 
 // Close the database connection
